d83d20addf41818a189cd37373288b70d9985946
Sai Gottemukkula <saigthefirst@gmail.com>
2020-10-22 19:59:33 +0000

    FINAL VERSION

--- main.py
+++ main.py
@@ -4,17 +4,20 @@ width = int(input('Enter the width: '))
 height = int(input('Enter the height: '))
 window = int(input('Enter the number of windows: '))
 door = int(input('Enter the number of doors: '))
-# Wall Dimentions (4 In Total)
-wall1 = length * height
-wall2 = height * width
-# Finding The Amount Of Paint Needed
-total_area = 2 * (wall1 + wall2)
-# ^ Total Area (Including Windows And Doors)
-final_area = total_area - 15 * window - 21 * door
-# One Gallon-Bucket Of Paint Covers 400 Square Feet
-buckets = int(final_area / 400)
-# ^ Gallons Of Paint
-# Rounding Up If Needed
-if final_area % 400 != 0:
-  buckets = buckets + 1
-print('You Need', buckets, 'Gallon-Buckets Of Paint')
+if length > 0 and width > 0 and height > 0:
+  # Wall Dimensions (4 In Total)
+  wall1 = length * height
+  wall2 = height * width
+  # Finding The Amount Of Paint Needed
+  total_area = 2 * (wall1 + wall2)
+  # ^ Total Area (Including Windows And Doors)
+  final_area = total_area - 15 * window - 21 * door
+  # One Gallon-Bucket Of Paint Covers 400 Square Feet
+  buckets = int(final_area / 400)
+  # ^ Gallons Of Paint
+  # Rounding Up If Needed
+  if final_area % 400 != 0:
+    buckets = buckets + 1
+  print('You Need', buckets, 'Gallon-Buckets Of Paint')
+else:
+  print('The Values For The Length, Width, And Height Have To Be Greater Than Zero')
